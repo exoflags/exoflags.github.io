7bfa0bd58b809ba1bb3e252b4de7a9735329099d
alexisdefoe <alexis.wilkinson-defoe@arup.com>
2019-09-09 15:06:12 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f3f23fa91eb8403f2fe2899d8264b1c.js"
+  "/precache-manifest.24d5bc27ab8d7b73b5d714f1b7ea0d3f.js"
 );
 
 self.addEventListener('message', (event) => {
